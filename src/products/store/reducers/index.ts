ba07f75be457566c864b158ca4fd30a590ebca33
Mac <macshah141@gmail.com>
2018-04-10 08:48:11 +0530

    dummy ngrx project

---
+++ src/products/store/reducers/index.ts
@@ -0,0 +1,39 @@
+import { ActionReducerMap, createFeatureSelector, createSelector } from '@ngrx/store';
+
+import * as fromPizzas from './pizzas.reducer';
+
+export interface ProductsState {
+  pizzas: fromPizzas.PizzaState
+}
+
+export const reducers: ActionReducerMap<ProductsState> = {
+  pizzas: fromPizzas.reducer
+};
+
+export const getProductsState = createFeatureSelector<ProductsState>(
+  'products'
+);
+
+// pizza state
+
+export const getPizzaState = createSelector(
+  getProductsState, 
+  (state: ProductsState) => state.pizzas,
+);
+
+export const getAllPizzas = createSelector(
+  getPizzaState,
+  fromPizzas.getPizzas
+);
+
+export const getAllPizzasLoaded = createSelector(
+  getPizzaState,
+  fromPizzas.getPizzasLoaded
+);
+
+export const getPizzasLoading = createSelector(
+  getPizzaState,
+  fromPizzas.getPizzasLoading
+);
+
+
